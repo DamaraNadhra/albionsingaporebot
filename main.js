0cf1be742d53a5630c5056f9da94684256cda036
DamaraNadhra <70007198+DamaraNadhra@users.noreply.github.com>
2021-08-08 13:17:06 +0700

    Update main.js

--- main.js
+++ main.js
@@ -614,6 +614,7 @@ client.on("interactionCreate", async (interaction) => {
       });
     }
   } else if (interaction.isCommand()) {
+    if (!client.interactionCommand.get(interaction.commandName)) return;
     try {
       await client.interactionCommand
         .get(interaction.commandName)
