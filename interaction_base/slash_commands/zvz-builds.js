f99792e6a7da992b4928c8f777281992575264d0
DamaraNadhra <70007198+DamaraNadhra@users.noreply.github.com>
2021-08-09 13:46:01 +0700

    Update zvz-builds.js

--- interaction_base/slash_commands/zvz-builds.js
+++ interaction_base/slash_commands/zvz-builds.js
@@ -22,69 +22,72 @@ module.exports = {
       interaction.member.roles.cache.has("759793776439984170") |
       interaction.member.permissions.has("ADMINISTRATOR")
     ) {
-      const buildType = interaction.options.getString('builds')
+      const buildType = interaction.options.getString("builds");
       if (buildType) {
         Object.keys(zvzlist).forEach((m, i) => {
           Object.keys(zvzlist[m]).forEach((element, index) => {
             if (element.includes(buildType)) {
               const build = zvzlist[m][element];
-                let zvzlistButton = new MessageButton()
-                  .setCustomId("listbuttonzvz")
-                  .setStyle("SUCCESS")
-                  .setLabel("Look for more builds!")
-                  .setEmoji("<:jennielove:844893922634235904>");
-                let referenceButton = new MessageButton()
-                  .setStyle("LINK")
-                  .setURL(build.reference)
-                  .setLabel("ARCH main discord reference");
-                let embed = new MessageEmbed()
-                  .setAuthor(build.label, build.pic)
-                  .setColor("ORANGE")
-                  .setImage(build.icon);
-                const embedDesc = new MessageEmbed()
-                  .setColor("ORANGE")
-                  .setDescription(
-                    `**Shortcut:** \`!${element}\` \n**Requirement:** 1100+ IP`
-                  )
-                  .setFooter(
-                    `Requested by ${
-                      interaction.guild.members.cache.get(interaction.user.id).displayName
-                    }`,
-                    interaction.user.displayAvatarURL()
-                  );
-                interaction.reply({
-                  content: "Processing...",
-                  embeds: [embed, embedDesc],
-                  components: [
-                    new MessageActionRow().addComponents(
-                      zvzlistButton,
-                      referenceButton
-                    ),
-                  ],
-                });
-              }
+              let zvzlistButton = new MessageButton()
+                .setCustomId("listbuttonzvz")
+                .setStyle("SUCCESS")
+                .setLabel("Look for more builds!")
+                .setEmoji("<:jennielove:844893922634235904>");
+              let referenceButton = new MessageButton()
+                .setStyle("LINK")
+                .setURL(build.reference)
+                .setLabel("ARCH main discord reference");
+              let embed = new MessageEmbed()
+                .setAuthor(build.label, build.pic)
+                .setColor("ORANGE")
+                .setImage(build.icon);
+              const embedDesc = new MessageEmbed()
+                .setColor("ORANGE")
+                .setDescription(
+                  `**Shortcut:** \`!${element}\` \n**Requirement:** 1100+ IP`
+                )
+                .setFooter(
+                  `Requested by ${
+                    interaction.guild.members.cache.get(interaction.user.id)
+                      .displayName
+                  }`,
+                  interaction.user.displayAvatarURL()
+                );
+              interaction.reply({
+                content: "Processing...",
+                embeds: [embed, embedDesc],
+                components: [
+                  new MessageActionRow().addComponents(
+                    zvzlistButton,
+                    referenceButton
+                  ),
+                ],
+              });
+            }
           });
         });
-      } else 
-      const deleteButton = new MessageButton()
-        .setStyle("DANGER")
-        .setCustomId("delete")
-        .setLabel("Delete")
-        .setEmoji("🚨");
-      await interaction.reply({
-        content: "ZvZ build List! according to ARCH official zvz builds",
-        components: [
-          row,
-          tankRow,
-          healRow,
-          new MessageActionRow().addComponents(deleteButton),
-        ],
-      });
+      } else {
+        const deleteButton = new MessageButton()
+          .setStyle("DANGER")
+          .setCustomId("delete")
+          .setLabel("Delete")
+          .setEmoji("🚨");
+        await interaction.reply({
+          content: "ZvZ build List! according to ARCH official zvz builds",
+          components: [
+            row,
+            tankRow,
+            healRow,
+            new MessageActionRow().addComponents(deleteButton),
+          ],
+        });
+      }
     } else {
       interaction.reply({
-        content: 'To prevent clutter, please redo this command at the bot commands channel,',
+        content:
+          "To prevent clutter, please redo this command at the bot commands channel,",
         ephemeral: true,
-      })
+      });
     }
   },
 };
